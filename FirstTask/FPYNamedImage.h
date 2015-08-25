afb088703cb1971884e9d37ce1b9e3e3b0654edc
Olga Khomich <olga.khomich13@gmail.com>
2015-08-25 22:28:19 +0300

    conflict previous commit

---
+++ FirstTask/FPYNamedImage.h
@@ -0,0 +1,17 @@
+//
+//  FPYNamedImage.h
+//  FirstProjectYalantis
+//  Copyright © 2015 Olga Khomich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FPYNamedImage : NSObject
+
+@property (nonatomic, readonly) NSString *title;
+@property (nonatomic, readonly) UIImage *image;
+
+-(instancetype)initWithImageName:(NSString *)initialImageName title:(NSString *)initialTitle;
+
+@end
